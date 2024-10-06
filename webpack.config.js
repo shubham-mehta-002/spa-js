11b5ce35d373706cd708939a414a23992f37ec2d
shubham-mehta-002 <mehta.shubham002@gmail.com>
2024-10-06 12:16:39 +0530

    build

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
+// webpack.config.js
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js', // Entry point
+    entry: './src/index.js', // Your main file
     output: {
         filename: 'router.bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        library: 'MyRouter',
-        libraryTarget: 'umd',
-        globalObject: 'this'
-    },    
+        library: {
+            name: 'MyRouter', // The name of your library
+            type: 'umd',      // Universal Module Definition
+        },
+        globalObject: 'this' // Ensures compatibility in different environments
+    },
     mode: 'production', // Set mode to production
 };
